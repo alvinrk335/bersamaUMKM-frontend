d9a07b27b8f26cfc928402af1083cde2593578d9
alvinrk335 <alvinrusellkurniawan05@gmail.com>
2025-10-23 13:11:43 +0700

    kerjaan dapian

---
+++ src/components/navbar.tsx
@@ -0,0 +1,11 @@
+import "./navbar.css";
+
+function Navbar() {
+  return (
+    <div className="container">
+      <nav className="nav-container"></nav>
+    </div>
+  );
+}
+
+export default Navbar;
