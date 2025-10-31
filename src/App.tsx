7c21504fb22e6e87fc06cbcfde005ef7cfb52152
Vyann412 <daviangeraldy@gmail.com>
2025-10-31 13:29:47 +0700

    lupa add apa aja cuman ingetnya navbar

--- src/App.tsx
+++ src/App.tsx
@@ -7,22 +7,19 @@ import routes from "./routes/route.tsx";
 function App() {
   const navigate = useNavigate();
   const elements = useRoutes(routes);
+  //unused
   const location = useLocation();
 
   useEffect(() => {
     navigate("/home");
   }, []);
 
+  //unused
   const hideNavbarOn = ["/umkm/detail"];
   const shouldHideNavbar = hideNavbarOn.some((path) =>
     location.pathname.startsWith(path)
   );
-  return (
-    <div className="App">
-      {!shouldHideNavbar && <Navbar />}
-      {elements}
-    </div>
-  );
+  return <div className="App">{elements}</div>;
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,30 @@ import App from "./App.tsx";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "leaflet/dist/leaflet.css";
 import { BrowserRouter } from "react-router-dom";
+import Footer from "./components/footer/Footer.tsx";
+import Navbar from "./components/navbar.tsx";
+import { useLocation } from "react-router-dom";
+
+const AppWrapper = () => {
+  const location = useLocation();
+  const hideNavbarOn = ["/umkm/detail"];
+  const shouldHideNavbar = hideNavbarOn.some((path) =>
+    location.pathname.startsWith(path)
+  );
+
+  return (
+    <>
+      {!shouldHideNavbar && <Navbar />}
+      <App />
+      <Footer />
+    </>
+  );
+};
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <AppWrapper />
     </BrowserRouter>
   </StrictMode>
 );
