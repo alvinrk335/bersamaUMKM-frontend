e4c63260badc4a3196349c134a4a9d227604c74b
alvinrk335 <alvinrusellkurniawan05@gmail.com>
2025-11-02 17:08:27 +0700

    rollback on ui change

--- src/routes/route.tsx
+++ src/routes/route.tsx
@@ -1,19 +1,14 @@
 import type { RouteObject } from "react-router-dom";
 import Home from "../page/home/home";
-import HomeV2 from "../page/home/HomeV2";
 import UmkmDetail from "../page/UmkmDetail/UmkmDetail";
 
 const routes: RouteObject[] = [
   {
     path: "/",
-    element: <HomeV2 />,
+    element: <Home />,
   },
   {
     path: "/home",
-    element: <HomeV2 />,
-  },
-  {
-    path: "/home-old",
     element: <Home />,
   },
   {
